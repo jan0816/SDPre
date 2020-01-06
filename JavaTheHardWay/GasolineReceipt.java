c6f73edb8929f4985afc574fbc4776e4b0fd716d
Janice Ellsworth <janice.ellsworth2@gmail.com>
2020-01-06 16:21:46 -0700

    adding other java files

---
+++ JavaTheHardWay/GasolineReceipt.java
@@ -0,0 +1,16 @@
+public class GasolineReceipt {
+	public static void main( String [] args ) {
+		System.out.println( "+------------------------+" );
+		System.out.println( "|                        |" );
+		System.out.println( "|      COSTCO GAS        |" );
+		System.out.println( "|                        |" );
+		System.out.println( "| 2020-01-06  04:38PM    |" );
+		System.out.println( "|                        |" );
+		System.out.println( "| Gallons:       14.870  |" );
+		System.out.println( "| Price/gallon:  $ 2.849 |" );
+		System.out.println( "|                        |" );
+		System.out.println( "|Fuel total:     $42.36  |" );
+		System.out.println( "|                        |" );
+		System.out.println( "+------------------------+" );
+			}
+}
