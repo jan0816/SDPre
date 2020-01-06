bf946852afac911faabcdcce38b1de7046009ede
Janice Ellsworth <janice.ellsworth2@gmail.com>
2020-01-06 16:11:23 -0700

    finish jthw ex 1

---
+++ JavaTheHardWay/FirstProg.java
@@ -0,0 +1,6 @@
+public class FirstProg {
+	public static void main( String [] args ) {
+		System.out.println( "I, Janice Ellsworth, am determined to learn how to code." );
+		System.out.println( "Today's days is January 6th, 2020" );
+		}
+	}
