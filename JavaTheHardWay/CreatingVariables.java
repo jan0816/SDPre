9ccfba3c3ab26b399d2ad389e0cbc6c766ecf658
Janice Ellsworth <janice.ellsworth2@gmail.com>
2020-01-06 19:04:47 -0700

    Variables

---
+++ JavaTheHardWay/CreatingVariables.java
@@ -0,0 +1,36 @@
+public class CreatingVariables {
+  public static void main( String [] args ){
+    int x, y, age, z;
+    double seconds, e, times, checking;
+    String firstName, lastName, title, wifeName, pets;
+
+    x = 10;
+    y = 400;
+    age = 39;
+    z = 222;
+
+    seconds = 4.71;
+    e = 2.71828182845904523536;
+    times = -8192;
+    checking = 1.89;
+
+    firstName = "Graham";
+    lastName = "Mitchell";
+    title = "Mr.";
+    wifeName = "Elsa Kensington";
+    pets = "Pablo + Kobi";
+
+
+    System.out.println( "The variable x contains " + x );
+    System.out.println( "The value " + y + " is stored in the variable y." );
+    System.out.println( "The number " + age + "is the secret number of years he's lived." );
+    System.out.println( "The letter " + z + " is the age of that zoo animal." );
+    System.out.println( "The amount of times " + times + " I asked for a dog and got it." );
+    System.out.println( "The experiment took " + seconds + " seconds." );
+    System.out.println( "A favorite irrational # is Euler's number: " + e );
+    System.out.println( "Hopefully you have more than $" + checking + "!" );
+    System.out.println( "My name's " + title + " " + firstName +lastName );
+    System.out.println( "He is married to " + wifeName + "." );
+    System.out.println( "They have two dogs named " + pets + "." );
+  }
+}
