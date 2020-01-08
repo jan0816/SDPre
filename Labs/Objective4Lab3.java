5dbf1c2c4f85df2c661d0470da7f68bfbc74d0ab
Janice Ellsworth <janice.ellsworth2@gmail.com>
2020-01-07 23:00:09 -0700

    O4L3

---
+++ Labs/Objective4Lab3.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Objective4Lab3 {
+    public static void main(String[] args) {
+      Scanner keyboard = new Scanner(System.in);
+      int userNum;
+
+      System.out.println(" Please enter a number: " );
+      userNum = keyboard.nextInt();
+      System.out.println(" The number is positive " );
+
+      keyboard.close();
+  }
+}
+//Perhaps I'm just really tired at this point and don't understand the instructions clearly but I got the code to match but didn't determine if the number is greater than, less than, or equal to 0.
