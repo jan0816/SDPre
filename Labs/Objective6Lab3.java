74ee3f30102d29d8d9164fcaf62714596b384bd9
Janice Ellsworth <janice.ellsworth2@gmail.com>
2020-01-12 12:31:28 -0700

    20oddeven

---
+++ Labs/Objective6Lab3.java
@@ -0,0 +1,20 @@
+public class Objective6Lab3 {
+  public static void main(String[] args) {
+    int counter = 1;
+    int MAX = 20;
+    boolean odd,even;
+
+    System.out.println(counter);
+
+      while ( counter <= 19 ) {
+          counter = counter + 1;
+          if (counter % 2 == 0) {
+          System.out.println( counter + " is even");
+        }
+        if (counter % 2 != 0) {
+        System.out.println( counter + " is odd");
+        }
+      }
+    }
+}
+//code works but doesn't show odd next to number 1..
