1c8cb46f9e3eea68e9c5034933163746c9de0ba4
Janice Ellsworth <janice.ellsworth2@gmail.com>
2020-01-13 12:57:51 -0700

    210

---
+++ Labs/Objective6Lab4.java
@@ -0,0 +1,11 @@
+public class Objective6Lab4{
+  public static void main(String [] args ){
+    int counter = 0, currentsumtotal = 0;
+
+    while (counter <= 20 ){
+      currentsumtotal += counter;
+      counter ++;
+    }
+    System.out.println(currentsumtotal);
+  }
+}
