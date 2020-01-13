38717c43948e5846865840dcf5f2652feb159c88
Janice Ellsworth <janice.ellsworth2@gmail.com>
2020-01-13 12:48:32 -0700

    O6L3 MOD

--- Labs/Objective6Lab3.java
+++ Labs/Objective6Lab3.java
@@ -1,20 +1,14 @@
 public class Objective6Lab3 {
   public static void main(String[] args) {
     int counter = 1;
-    int MAX = 20;
-    boolean odd,even;
 
-    System.out.println(counter + " is odd");
-
-      while ( counter <= 19 ) {
+      while ( counter <= 20 ) {
+          if (counter % 2 == 0)
+            System.out.println( counter + " is even");
+          else {
+            System.out.println( counter + " is odd");
+          }
           counter = counter + 1;
-          if (counter % 2 == 0) {
-          System.out.println( counter + " is even");
-        }
-        if (counter % 2 != 0) {
-        System.out.println( counter + " is odd");
-        }
       }
     }
-}
-//code works but doesn't show odd next to number 1..
+  }
